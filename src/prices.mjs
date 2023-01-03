91db923ee1f8a72a0f9aeb219c4b50725bb3bf52
Juho Taipale <krooh@hotmail.com>
2023-01-03 08:35:43 +0200

    in isholiday holiday to plaindate

--- src/prices.mjs
+++ src/prices.mjs
@@ -90,12 +90,12 @@ function createApp(database) {
   function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-      let holiday = new Date(row.holiday);
+      let holiday = Temporal.PlainDate.from(row.holiday);
       if (
         date &&
-        date.year === holiday.getFullYear() &&
-        date.month === holiday.getMonth() &&
-        date.day === holiday.getDate()
+        date.year === holiday.year &&
+        date.month === holiday.montht &&
+        date.day === holiday.day
       ) {
         return true;
       }
