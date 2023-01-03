ba8a275295c0202fce95545b5d6faa149dfd4690
Juho Taipale <krooh@hotmail.com>
2023-01-03 08:38:12 +0200

    redundant variable removed

--- src/prices.mjs
+++ src/prices.mjs
@@ -18,8 +18,7 @@ function createApp(database) {
     const age = req.query.age;
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
-    let ds = req.query.date
-    const date = parsePlainDate(ds);
+    const date = parsePlainDate(req.query.date);
     const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
